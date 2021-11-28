b426f5bc2bc276ac9f6839bfa515af0c5708d8d6
Lluís Camino <caminoperez.lluis@gmail.com>
2021-11-28 20:55:08 +0100

    Apply collinear features removal to sgd_log and sgd_perceptron

--- models/sgd_perceptron.py
+++ models/sgd_perceptron.py
@@ -1,9 +1,12 @@
 from sklearn.linear_model import SGDClassifier
 from sklearn.model_selection import train_test_split
 
+from feature_selection.filter_method import remove_collinear_features
 from utils.utils import get_data, scale_data, generate_submission, print_model_performance_metrics
 
 X, y = get_data()
+
+to_drop = remove_collinear_features(X, 0.60)
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=57)
 
@@ -15,4 +18,4 @@ prediction = clf.predict(X_test)
 
 print_model_performance_metrics(y_test, prediction)
 
-generate_submission(clf, "output.csv")
+generate_submission(clf, "output.csv", to_drop)
