4e636ae04f5dbbc70e678109ae1cc22cf1d231ac
lluis <caminoperez.lluis@gmail.com>
2021-11-29 19:13:56 +0100

    Fix path in join_csvs

--- utils/join_csvs.py
+++ utils/join_csvs.py
@@ -5,7 +5,7 @@ import pandas as pd
 parser = ArgumentParser()
 parser.add_argument("-o", "--output", dest="output_file",
                     help="write result CSV to FILE", metavar="FILE",
-                    default="data/train.csv")
+                    default="../data/train.csv")
 parser.add_argument("-t", "--type", dest="type",
                     help="train or test", metavar="TYPE",
                     default="train")
@@ -13,11 +13,11 @@ args = parser.parse_args()
 
 delimiter = "," if args.type == "train" else ";"
 
-info_data = pd.read_csv(f"data/{args.type}/info_{args.type}.csv", delimiter=";", decimal=",")
-color_data = pd.read_csv(f"data/{args.type}/color_{args.type}.csv", delimiter=delimiter, decimal=",") \
+info_data = pd.read_csv(f"../data/{args.type}/info_{args.type}.csv", delimiter=";", decimal=",")
+color_data = pd.read_csv(f"../data/{args.type}/color_{args.type}.csv", delimiter=delimiter, decimal=",") \
     .drop(columns="Class")
-shape_data = pd.read_csv(f"data/{args.type}/shape_{args.type}.csv", delimiter=delimiter, decimal=",")
-texture_data = pd.read_csv(f"data/{args.type}/texture_{args.type}.csv", delimiter=delimiter, decimal=",") \
+shape_data = pd.read_csv(f"../data/{args.type}/shape_{args.type}.csv", delimiter=delimiter, decimal=",")
+texture_data = pd.read_csv(f"../data/{args.type}/texture_{args.type}.csv", delimiter=delimiter, decimal=",") \
     .drop(columns="Class")
 
 data = info_data.join(color_data).join(shape_data).join(texture_data)
