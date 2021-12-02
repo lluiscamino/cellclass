81499c12f829bdd2ea67e097ef3bdb917856049e
Lluís Camino <caminoperez.lluis@gmail.com>
2021-12-02 14:47:29 +0100

    Add hyperparameter tuning for SVM

--- models/svm.py
+++ models/svm.py
@@ -9,10 +9,15 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_
 
 X_train, X_test = scale_data(X_train, X_test)
 
-svc = SVC(C=1.0, kernel="linear", probability=True)
-svc.fit(X_train, y_train)
-prediction = svc.predict(X_test)
+kernels = ["poly", "sigmoid"]
+c_values = [1, 10, 100, 1000, 10000]
 
-print_model_performance_metrics(y_test, prediction)
-
-generate_submission(svc, "output.csv")
+for kernel in kernels:
+    print("\nSVC kernel=", kernel)
+    for c in c_values:
+        print("C=", c)
+        svc = SVC(C=c, kernel=kernel, probability=True)
+        svc.fit(X_train, y_train)
+        prediction = svc.predict(X_test)
+        print_model_performance_metrics(y_test, prediction)
+        generate_submission(svc, f"output_{kernel}_c={c}.csv")
