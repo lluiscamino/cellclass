6bd2107de854b3650d374edebc9a08cfbc97ecd7
Lluís Camino <caminoperez.lluis@gmail.com>
2021-12-02 13:24:19 +0100

    learning_rate=optimal

--- models/sgd_perceptron.py
+++ models/sgd_perceptron.py
@@ -12,7 +12,7 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_
 
 X_train, X_test = scale_data(X_train, X_test)
 
-clf = SGDClassifier(loss="perceptron", eta0=1, max_iter=1000, learning_rate="constant", random_state=5)
+clf = SGDClassifier(loss="perceptron", eta0=1, max_iter=1000, random_state=5)
 clf.fit(X_train, y_train)
 prediction = clf.predict(X_test)
 
